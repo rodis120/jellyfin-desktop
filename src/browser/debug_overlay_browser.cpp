2692263ed919e3805999d4a5b5fcf0c8b0c33155
Rogal <progalin55@gmail.com>
2026-05-08 23:06:53 +0200

    Split into sections

--- src/browser/debug_overlay_browser.cpp
+++ src/browser/debug_overlay_browser.cpp
@@ -175,7 +175,7 @@ std::string qoute_string(std::string_view s) {
 
 CefRefPtr<CefDictionaryValue> DebugOverlayBrowser::injectionProfile() {
     static const char* const kFunctions[] = {
-        "update"
+        "update", "init"
     };
     static const char* const kScripts[] = {};
     CefRefPtr<CefListValue> fns = CefListValue::Create();
@@ -217,104 +217,128 @@ bool DebugOverlayBrowser::handleMessage(const std::string& name,
                                  CefRefPtr<CefListValue> args,
                                  CefRefPtr<CefBrowser> browser) {
     if (name == "update") {
-        std::stringstream debug_info;
-        debug_info << "Jellyfin\n";
-        debug_info << "Version: " << APP_VERSION_FULL " (built " __DATE__ " " __TIME__ << ")\n";
-        debug_info << "CEF Version: " << APP_CEF_VERSION << "\n";
-        debug_info << "User agent: " << APP_USER_AGENT << "\n";
-        debug_info << "Platform: " << APP_PLATFORM << "\n";
+        update_misc();
+        update_window();
+        update_video();
 
-        debug_info << "\nFiles:\n";
-        debug_info << "  Config file: " << Settings::instance().getConfigPath() << "\n";
-
-        debug_info << "\nNetwork addresses:\n";
-        for (auto& addr : networkAddresses()) {
-            debug_info << addr << "\n";
-        }
-
-        debug_info << "\nClient settings:\n";
-        debug_info << Settings::instance().cliSettingsJson() << "\n";
-
-        debug_info << "\nWindow:\n";
-        debug_info << "Display backend: " << displayBackendToString(g_platform.display) << "\n";
-        debug_info << "Physical size: " << g_browsers->physical_w() << "x" << g_browsers->physical_h() << "\n";
-        debug_info << "Logical size: " << g_browsers->logical_w() << "x" << g_browsers->logical_h() << "\n";
-        debug_info << "Scale: " << mpv::display_scale() << "\n";
-
-        std::stringstream video_info;
-        video_info << "File:\n";
-        video_info << "URL: " << MPV_PROPERTY("path") << "\n";
-        video_info << "Container: " << MPV_PROPERTY("file-format") << "\n";
-        video_info << "Native seeking: " << ((MPV_PROPERTY_BOOL("seekable") &&
-                                        !MPV_PROPERTY_BOOL("partially-seekable"))
-                                        ? "yes" : "no") << "\n";
-        video_info << "\n";
-        video_info << "Video:\n";
-        video_info << "Codec: " << MPV_PROPERTY("video-codec") << "\n";
-        video_info << "Size: " << MPV_PROPERTY("video-params/dw") << "x"
-                        << MPV_PROPERTY("video-params/dh") << "\n";
-        video_info << "FPS (container): " << MPV_PROPERTY("container-fps") << "\n";
-        video_info << "FPS (filters): " << MPV_PROPERTY("estimated-vf-fps") << "\n";
-        video_info << "Aspect: " << MPV_PROPERTY("video-params/aspect") << "\n";
-        video_info << "Bitrate: " << MPV_PROPERTY("video-bitrate") << "\n";
-        video_info << "Display FPS: " << MPV_PROPERTY("display-fps") << "\n";
-        video_info << "Hardware Decoding: " << MPV_PROPERTY("hwdec-current")
-                                        << " (" << MPV_PROPERTY("hwdec-interop") << ")\n";
-        video_info << "\n";
-        video_info << "Audio:\n";
-        video_info << "Codec: " << MPV_PROPERTY("audio-codec") << "\n";
-        video_info << "Bitrate: " << MPV_PROPERTY("audio-bitrate") << "\n";
-        video_info << "Channels: ";
-        appendAudioFormat(video_info, "audio-params");
-        video_info << " -> ";
-        appendAudioFormat(video_info, "audio-out-params");
-        video_info << "\n";
-        video_info << "Output driver: " << MPV_PROPERTY("current-ao") << "\n";
-        video_info << "\n";
-        video_info << "Performance:\n";
-        video_info << "A/V: " << MPV_PROPERTY("avsync") << "\n";
-        video_info << "Dropped frames: " << MPV_PROPERTY("vo-drop-frame-count") << "\n";
-        bool dispSync = MPV_PROPERTY_BOOL("display-sync-active");
-        video_info << "Display Sync: ";
-        if (!dispSync)
-        {
-            video_info << "no\n";
-        }
-        else
-        {
-            video_info << "yes (ratio " << MPV_PROPERTY("vsync-ratio") << ")\n";
-            video_info << "Mistimed frames: " << MPV_PROPERTY("mistimed-frame-count")
-                                        << "/" << MPV_PROPERTY("vo-delayed-frame-count") << "\n";
-            video_info << "Measured FPS: " << MPV_PROPERTY("estimated-display-fps")
-                                    << " (" << MPV_PROPERTY("vsync-jitter") << ")\n";
-            video_info << "V. speed corr.: " << MPV_PROPERTY("video-speed-correction") << "\n";
-            video_info << "A. speed corr.: " << MPV_PROPERTY("audio-speed-correction") << "\n";
-        }
-        video_info << "\n";
-        video_info << "Cache:\n";
-        video_info << "Seconds: " << MPV_PROPERTY("demuxer-cache-duration") << "\n";
-        video_info << "Extra readahead: " << MPV_PROPERTY("cache-used") << "\n";
-        video_info << "Buffering: " << MPV_PROPERTY("cache-buffering-state") << "\n";
-        video_info << "Speed: " << MPV_PROPERTY("cache-speed") << "\n";
-        video_info << "\n";
-        video_info << "Misc:\n";
-        video_info << "Time: " << MPV_PROPERTY("playback-time") << " / "
-                        << MPV_PROPERTY("duration")
-                        << " (" << MPV_PROPERTY("percent-pos") << "%)\n";
-        video_info << "State: " << (MPV_PROPERTY_BOOL("pause") ? "paused " : "")
-                            << (MPV_PROPERTY_BOOL("paused-for-cache") ? "buffering " : "")
-                            << (MPV_PROPERTY_BOOL("core-idle") ? "waiting " : "playing ")
-                            << (MPV_PROPERTY_BOOL("seeking") ? "seeking " : "")
-                            << "\n";
-
-        layer_->execJs("window.set_debug_info(" + qoute_string(debug_info.str()) + ");");
-        layer_->execJs("window.set_video_info(" + qoute_string(video_info.str()) + ");");
+        return true;
+    } else if(name == "init") {
+        update_app();
+        update_misc();
+        update_window();
+        update_video();
 
         return true;
     }
     return false;
 }
 
+void DebugOverlayBrowser::update_app() {
+    std::stringstream debug_info;
+    debug_info << "Jellyfin\n";
+    debug_info << "Version: " << APP_VERSION_FULL " (built " __DATE__ " " __TIME__ << ")\n";
+    debug_info << "CEF Version: " << APP_CEF_VERSION << "\n";
+    debug_info << "User agent: " << APP_USER_AGENT << "\n";
+    debug_info << "Platform: " << APP_PLATFORM << "\n";
+
+    set_info("app_info", debug_info.str());
+}
+
+void DebugOverlayBrowser::update_misc() {
+    std::stringstream debug_info;
+    debug_info << "Network addresses:\n";
+    for (auto& addr : networkAddresses()) {
+        debug_info << addr << "\n";
+    }
+
+    debug_info << "\nClient settings:\n";
+    debug_info << Settings::instance().cliSettingsJson() << "\n";
+
+    set_info("misc_info", debug_info.str());
+}
+
+void DebugOverlayBrowser::update_window() {
+    std::stringstream debug_info;
+    debug_info << "Window:\n";
+    debug_info << "Display backend: " << displayBackendToString(g_platform.display) << "\n";
+    debug_info << "Display names: " << MPV_PROPERTY("display-names") << "\n";
+    debug_info << "Physical size: " << g_browsers->physical_w() << "x" << g_browsers->physical_h() << "\n";
+    debug_info << "Logical size: " << g_browsers->logical_w() << "x" << g_browsers->logical_h() << "\n";
+    debug_info << "Scale: " << mpv::display_scale() << "\n";
+
+    set_info("window_info", debug_info.str());
+}
+
+void DebugOverlayBrowser::update_video() {
+    std::stringstream video_info;
+    video_info << "File:\n";
+    video_info << "URL: " << MPV_PROPERTY("path") << "\n";
+    video_info << "Container: " << MPV_PROPERTY("file-format") << "\n";
+    video_info << "Native seeking: " << ((MPV_PROPERTY_BOOL("seekable") &&
+                                    !MPV_PROPERTY_BOOL("partially-seekable"))
+                                    ? "yes" : "no") << "\n";
+    video_info << "\n";
+    video_info << "Video:\n";
+    video_info << "Codec: " << MPV_PROPERTY("video-codec") << "\n";
+    video_info << "Size: " << MPV_PROPERTY("video-params/dw") << "x"
+                    << MPV_PROPERTY("video-params/dh") << "\n";
+    video_info << "FPS (container): " << MPV_PROPERTY("container-fps") << "\n";
+    video_info << "FPS (filters): " << MPV_PROPERTY("estimated-vf-fps") << "\n";
+    video_info << "Aspect: " << MPV_PROPERTY("video-params/aspect") << "\n";
+    video_info << "Bitrate: " << MPV_PROPERTY("video-bitrate") << "\n";
+    video_info << "Display FPS: " << MPV_PROPERTY("display-fps") << "\n";
+    video_info << "Hardware Decoding: " << MPV_PROPERTY("hwdec-current")
+                                    << " (" << MPV_PROPERTY("hwdec-interop") << ")\n";
+    video_info << "\n";
+    video_info << "Audio:\n";
+    video_info << "Codec: " << MPV_PROPERTY("audio-codec") << "\n";
+    video_info << "Bitrate: " << MPV_PROPERTY("audio-bitrate") << "\n";
+    video_info << "Channels: ";
+    appendAudioFormat(video_info, "audio-params");
+    video_info << " -> ";
+    appendAudioFormat(video_info, "audio-out-params");
+    video_info << "\n";
+    video_info << "Output driver: " << MPV_PROPERTY("current-ao") << "\n";
+    video_info << "\n";
+    video_info << "Performance:\n";
+    video_info << "A/V: " << MPV_PROPERTY("avsync") << "\n";
+    video_info << "Dropped frames: " << MPV_PROPERTY("vo-drop-frame-count") << "\n";
+    bool dispSync = MPV_PROPERTY_BOOL("display-sync-active");
+    video_info << "Display Sync: ";
+    if (!dispSync) {
+        video_info << "no\n";
+    } else {
+        video_info << "yes (ratio " << MPV_PROPERTY("vsync-ratio") << ")\n";
+        video_info << "Mistimed frames: " << MPV_PROPERTY("mistimed-frame-count")
+                                    << "/" << MPV_PROPERTY("vo-delayed-frame-count") << "\n";
+        video_info << "Measured FPS: " << MPV_PROPERTY("estimated-display-fps")
+                                << " (" << MPV_PROPERTY("vsync-jitter") << ")\n";
+        video_info << "V. speed corr.: " << MPV_PROPERTY("video-speed-correction") << "\n";
+        video_info << "A. speed corr.: " << MPV_PROPERTY("audio-speed-correction") << "\n";
+    }
+    video_info << "\n";
+    video_info << "Cache:\n";
+    video_info << "Seconds: " << MPV_PROPERTY("demuxer-cache-duration") << "\n";
+    video_info << "Extra readahead: " << MPV_PROPERTY("cache-used") << "\n";
+    video_info << "Buffering: " << MPV_PROPERTY("cache-buffering-state") << "\n";
+    video_info << "Speed: " << MPV_PROPERTY("cache-speed") << "\n";
+    video_info << "\n";
+    video_info << "Misc:\n";
+    video_info << "Time: " << MPV_PROPERTY("playback-time") << " / "
+                    << MPV_PROPERTY("duration")
+                    << " (" << MPV_PROPERTY("percent-pos") << "%)\n";
+    video_info << "State: " << (MPV_PROPERTY_BOOL("pause") ? "paused " : "")
+                        << (MPV_PROPERTY_BOOL("paused-for-cache") ? "buffering " : "")
+                        << (MPV_PROPERTY_BOOL("core-idle") ? "waiting " : "playing ")
+                        << (MPV_PROPERTY_BOOL("seeking") ? "seeking " : "")
+                        << "\n";
+    
+    set_info("video_info", video_info.str());
+}
+
+void DebugOverlayBrowser::set_info(const std::string& id, const std::string& info) {
+    layer_->execJs("window.set_info(" + qoute_string(id) + "," + qoute_string(info) + ");");
+}
 
 void DebugOverlayBrowser::show() {
     if (s_self) {
--- src/browser/debug_overlay_browser.h
+++ src/browser/debug_overlay_browser.h
@@ -16,6 +16,13 @@ public:
 private:
     DebugOverlayBrowser();
 
+    void update_app();
+    void update_misc();
+    void update_window();
+    void update_video();
+
+    void set_info(const std::string& id, const std::string& info);
+
     bool handleMessage(const std::string& name,
                        CefRefPtr<CefListValue> args,
                        CefRefPtr<CefBrowser> browser);
